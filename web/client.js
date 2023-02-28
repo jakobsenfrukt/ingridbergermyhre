9061ff3d0a61c99b85f097d49afba12db9e5be9d
elisejakob <hei@elisejakob.no>
2023-02-28 10:20:33 +0900

    design progress, mobile adjustments, ensure fresh data

--- web/client.js
+++ web/client.js
@@ -4,5 +4,5 @@ export default sanityClient({
   projectId: 'vvl84ffe', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   apiVersion: '2022-11-11',
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: false // `false` if you want to ensure fresh data
 })
