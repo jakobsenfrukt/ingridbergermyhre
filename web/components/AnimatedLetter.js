e0aa27b9576aee713d1790242feb9f61cbd33dcc
elisejakob <hei@elisejakob.no>
2023-03-09 19:46:11 +0900

    add key to quotes, aniamtion fix in progress

--- web/components/AnimatedLetter.js
+++ web/components/AnimatedLetter.js
@@ -1,77 +1,87 @@
 import styles from './animatedTitle.module.scss'
+import { useState, useEffect } from 'react';
 
 export default function AnimatedLetter ({ letter, width }) {
-  const random = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  const style = function(width) {
-    if (width === 'narrow') {
-      return {
-        transitionDelay: `${random(0.2, 0.6)}s`,
-        transitionDuration: `${random(1, 3)}s`,
-        fontVariationSettings: `
-          'wght' 400, /* 100	to 1000 */
-          'wdth' 25, /* 25 to 151 */
-          'opsz' 10, /* 8 to 144 */
-          'GRAD' 0, /* -200 to 150 */
-          'slnt' 0, /* -10 to 0 */
-          'XTRA' 420, /* 323 to 603 */
-          'XOPQ' 90, /* 27 to 175 */
-          'YOPQ' 68, /* 25 to 135 */
-          'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
-          'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
-          'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
-          'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
-          'YTFI' 600 /* 560 to 788 */
-        `
+
+  const [style, setStyle] = useState(undefined)
+
+  useEffect(() => {
+    console.log('useEffect ran')
+
+    function random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    function calculateStyle(width) {
+      if (width === 'narrow') {
+        return {
+          transitionDelay: `${random(0.2, 0.6)}s`,
+          transitionDuration: `${random(1, 3)}s`,
+          fontVariationSettings: `
+            'wght' 400, /* 100	to 1000 */
+            'wdth' 25, /* 25 to 151 */
+            'opsz' 10, /* 8 to 144 */
+            'GRAD' 0, /* -200 to 150 */
+            'slnt' 0, /* -10 to 0 */
+            'XTRA' 420, /* 323 to 603 */
+            'XOPQ' 90, /* 27 to 175 */
+            'YOPQ' 68, /* 25 to 135 */
+            'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
+            'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
+            'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
+            'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
+            'YTFI' 600 /* 560 to 788 */
+          `
+        }
+      }
+      if (width === 'normal') {
+        return {
+          transitionDelay: `${random(0.2, 0.6)}s`,
+          transitionDuration: `${random(1, 3)}s`,
+          fontVariationSettings: `
+            'wght' 400, /* 100	to 1000 */
+            'wdth' 76, /* 25 to 151 */
+            'opsz' 10, /* 8 to 144 */
+            'GRAD' 0, /* -200 to 150 */
+            'slnt' 0, /* -10 to 0 */
+            'XTRA' 520, /* 323 to 603 */
+            'XOPQ' 90, /* 27 to 175 */
+            'YOPQ' 68, /* 25 to 135 */
+            'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
+            'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
+            'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
+            'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
+            'YTFI' 600 /* 560 to 788 */
+          `
+        }
+      }
+      if (width === 'wide') {
+        return {
+          transitionDelay: `${random(0.2, 0.6)}s`,
+          transitionDuration: `${random(0.6, 1)}s`,
+          fontVariationSettings: `
+            'wght' 400, /* 100	to 1000 */
+            'wdth' 151, /* 25 to 151 */
+            'opsz' 10, /* 8 to 144 */
+            'GRAD' 0, /* -200 to 150 */
+            'slnt' 0, /* -10 to 0 */
+            'XTRA' 603, /* 323 to 603 */
+            'XOPQ' 90, /* 27 to 175 */
+            'YOPQ' 68, /* 25 to 135 */
+            'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
+            'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
+            'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
+            'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
+            'YTFI' 600 /* 560 to 788 */
+          `
+        }
       }
     }
-    if (width === 'normal') {
-      return {
-        transitionDelay: `${random(0.2, 0.6)}s`,
-        transitionDuration: `${random(1, 3)}s`,
-        fontVariationSettings: `
-          'wght' 400, /* 100	to 1000 */
-          'wdth' 76, /* 25 to 151 */
-          'opsz' 10, /* 8 to 144 */
-          'GRAD' 0, /* -200 to 150 */
-          'slnt' 0, /* -10 to 0 */
-          'XTRA' 520, /* 323 to 603 */
-          'XOPQ' 90, /* 27 to 175 */
-          'YOPQ' 68, /* 25 to 135 */
-          'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
-          'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
-          'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
-          'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
-          'YTFI' 600 /* 560 to 788 */
-        `
-      }
-    }
-    if (width === 'wide') {
-      return {
-        transitionDelay: `${random(0.2, 0.6)}s`,
-        transitionDuration: `${random(1, 3)}s`,
-        fontVariationSettings: `
-          'wght' 400, /* 100	to 1000 */
-          'wdth' 151, /* 25 to 151 */
-          'opsz' 10, /* 8 to 144 */
-          'GRAD' 0, /* -200 to 150 */
-          'slnt' 0, /* -10 to 0 */
-          'XTRA' 603, /* 323 to 603 */
-          'XOPQ' 90, /* 27 to 175 */
-          'YOPQ' 68, /* 25 to 135 */
-          'YTLC' ${random(416, 500)}, /* 416 to 570 - lowercase height */
-          'YTUC' ${random(528, 660)}, /* 528 to 760 - uppercase height */
-          'YTAS' ${random(649, 700)}, /* 649 to 854 - ascender height */
-          'YTDE' ${random(-305, -200)}, /* -305 to -98 - descender height */
-          'YTFI' 600 /* 560 to 788 */
-        `
-      }
-    }
-  }
+
+    //setStyle(calculateStyle(width))
+  }, [])
 
   return (
-    <span style={style(width)}>{letter}</span>
+    <span style={style}>{letter}</span>
   )
 }
--- web/components/QuoteList.js
+++ web/components/QuoteList.js
@@ -5,9 +5,9 @@ export default function QuoteList ({ quotes }) {
     <div className={styles.quotes}>
       <ul className={styles.quotelist}>
         {quotes.length > 0 && quotes.map(
-          ({text, name, source, sourceUrl}) =>
+          ({text, name, source, sourceUrl}, index) =>
             text && (
-              <li className={styles.quote} key={name}>
+              <li className={styles.quote} key={`quote-${index}`}>
                 <p className={styles.text}>{text}</p>
                 <span className={styles.name}>{name}</span>
                 <a className={styles.source} href={sourceUrl}>{source}</a>
