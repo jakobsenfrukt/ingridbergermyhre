1cbca9e7f16f05c0f3a3ac7e3e21a154ea70b873
elisejakob <hei@elisejakob.no>
2023-04-28 15:38:23 +0200

    image optimization and animation fallback

--- web/components/AnimatedTitleSmall.js
+++ web/components/AnimatedTitleSmall.js
@@ -1,19 +1,26 @@
 import styles from './animatedTitle.module.scss'
-import { useInView } from 'react-intersection-observer'
-import AnimatedLetter from './AnimatedLetter'
 
 export default function AnimatedTitleSmall ({ content, width }) {
-  const { ref: observer, inView: isInView } = useInView({ triggerOnce: false })
 
   return (
-    <div className={`${styles.animatedTitle} ${styles[width]} ${styles.small}`} data-inview={isInView} key={content}>
-      {content.length > 0 && content.split("").map(
+    <div className={`${styles.animatedTitle} ${styles[width]} ${styles.small}`} key={content}>
+      {/* content.length > 0 && content.split("").map(
         (letter, index) =>
           (
             <span className={`animatedLetter ${width}`} key={index}><span>{letter}</span></span>
           )
+          ) */}
+
+      {content.length > 0 && content.split(" ").map(
+        (word, index) => (
+          <div className="animatedWord" key={index}>{word.split("").map(
+            (letter, index) =>
+              (
+                <span className={`animatedLetter ${width}`} key={index}><span>{letter}</span></span>
+              )
+              )}</div>
+        )
       )}
-      <div className={styles.observer} ref={observer}></div>
     </div>
   )
 }
--- web/components/Image.js
+++ web/components/Image.js
@@ -1,10 +1,6 @@
 import styles from './imageGallery.module.scss'
 import { useInView } from 'react-intersection-observer'
 
-/* testing */
-import Img from 'next/future/image';
-import { useNextSanityImage } from 'next-sanity-image';
-
 import client from '../client'
 import imageUrlBuilder from '@sanity/image-url'
 function urlFor (source) {
@@ -14,27 +10,19 @@ function urlFor (source) {
 export default function Image ({ image, showCredit }) {
   const { ref: observer, inView: IsInView } = useInView({ triggerOnce: false })
 
-  /* testing */
-  const imageProps = useNextSanityImage(client, image);
-
   return (
     <figure 
       data-inview={IsInView} 
       className={styles.image} 
     >
       <div className={styles.observer} ref={observer}></div>
-      {/*<img
+      <img
         src={urlFor(image)
           .width(1920)
           .url()}
         alt={image.alt}
         loading="lazy"
-        />*/}
-
-      <Img
-        {...imageProps}
-        alt={image.alt}
-      />
+        />
 
 
       {showCredit && image.credit && <figcaption className={styles.caption}>Photo: {image.credit}</figcaption>}
