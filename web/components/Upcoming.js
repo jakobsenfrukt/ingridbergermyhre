94bb1cda6ecc9e4effce74655e63024b4158c293
elisejakob <hei@elisejakob.no>
2024-05-09 01:13:27 +0200

    update list of upcoming dates, hide residencies, show cancelled with labels

--- web/components/Upcoming.js
+++ web/components/Upcoming.js
@@ -11,7 +11,7 @@ export default function Upcoming({ projects, limit }) {
     color: project.color
   }))).flat()
     .filter(i => isUpcoming(i.date.date))
-    .filter(i => i.date.status !== 'cancelled')
+    .filter(i => i.date.status !== 'in-residency')
     .sort((a, b) => (new Date(a.date.date) - new Date(b.date.date)))
 
   const [maxLimit, setMaxLimit] = useState(limit);
@@ -23,7 +23,10 @@ export default function Upcoming({ projects, limit }) {
     setMaxLimit(limit);
   }
 
-
+  function replaceHyphens(string) {
+    var newString = string.replace(/-/g, ' ')
+    return newString
+  }
 
   return (
     <div className={styles.upcoming}>
@@ -33,7 +36,11 @@ export default function Upcoming({ projects, limit }) {
           (event, index) =>
             event.date && (
               <li key={event.title + index} className={styles.upcoming} style={{ '--color-palette': event.color.hex }}>
-                {event.date.monthOnly ? <span className={styles.date}>{new Date(event.date.date).toLocaleDateString('en-GB', { month: 'long', year: 'numeric' })}</span> : <span className={styles.date}>{new Date(event.date.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</span>}
+                <div className={styles.date}>
+                  {event.date.monthOnly ? <span className={styles.date}>{new Date(event.date.date).toLocaleDateString('en-GB', { month: 'long', year: 'numeric' })}</span> : <span className={styles.date}>{new Date(event.date.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</span>}
+
+                  {event.date.status && event.date.status === 'cancelled' && <span className={styles.status} data-status={event.date.status}>{replaceHyphens(event.date.status)}</span>}
+                </div>
                 <span className={styles.title}>{event.title}</span>
                 {event.date.venue && event.date.city && <span className={styles.venue}>{event.date.venue}, {event.date.city}</span>}
                 {event.slug && <Link href="/projects/[slug]" as={`/projects/${event.slug.current}`}><a><span>More info</span></a></Link>}
