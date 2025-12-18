99e7073abb8106a1cf5dd79e09069daa858696ae
elise <hei@elisejakob.no>
2025-12-18 16:06:38 +0100

    open newsletter link in new window

--- web/components/Newsletter.js
+++ web/components/Newsletter.js
@@ -5,7 +5,7 @@ export default function Newsletter({ content }) {
     <section className={styles.newsletter}>
       <h1>Newsletter</h1>
       <p>{content.text}</p>
-      <a href="https://ingrid-14.kit.com/e4cc6c3349">Sign up to my newsletter <span className={styles.icon}>&rarr;</span></a>
+      <a href="https://ingrid-14.kit.com/e4cc6c3349" target="_blank">Sign up to my newsletter <span className={styles.icon}>&rarr;</span></a>
     </section>
   )
 }
