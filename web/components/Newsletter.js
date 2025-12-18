9a584fb4561d24ebe00f12f051bfed794dd93750
elise <hei@elisejakob.no>
2025-12-18 16:06:03 +0100

    add cli file, edit newsletter button hover effect

--- web/components/Newsletter.js
+++ web/components/Newsletter.js
@@ -5,7 +5,7 @@ export default function Newsletter({ content }) {
     <section className={styles.newsletter}>
       <h1>Newsletter</h1>
       <p>{content.text}</p>
-      <a href="https://ingrid-14.kit.com/e4cc6c3349">Sign up to my newsletter</a>
+      <a href="https://ingrid-14.kit.com/e4cc6c3349">Sign up to my newsletter <span className={styles.icon}>&rarr;</span></a>
     </section>
   )
 }
